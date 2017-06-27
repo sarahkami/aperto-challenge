687995a16d7928f8af828221e82f1f086fc586e8
Sarah Ganter <sa.ganter@web.de>
2017-06-28 00:37:33 +0200

    add comment

--- modal.js
+++ modal.js
@@ -13,3 +13,10 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+/**
+ *  Mir ist aufgefallen, dass diese Funktion nur für das erste Bild funktionieren kann, da ich ja mit eindeutigen Id's arbeite.
+ *  Der einfachste Weg wäre es, die Funktion einfach 3x zu schreiben, aber das wäre sehr unschöner Stil.
+ *  Vermutich könnte man ein Array erstellen, welches durch die drei Id's geht und guckt, worauf geklickt wurde? Und dementsprechend, das ausgewählte Bild anzeigt (?)
+ *  Ebenfalls hatte ich Probleme damit, den Button auf jedem Bild mittig zu platzieren. Auch hier könnte ich natürlich verschiedene Klassen vergeben, habe mich aber dagegen entschieden (schlechter Stil?). Ich hoffe es gibt eine Möglichkeit eine generelle Klasse für "automatisch" mittig platzierte Kind-Objekte (z.B. Button) in einem Eltern-Objekt (Div) zu vergeben (?)
+ */
